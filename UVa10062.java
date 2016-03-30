6888e6bba40ae4f11868eeef67ab6ce28e65fe0c
maxi67 <gckc3130@gmail.com>
2016-03-30 23:45:42 +0800

    Create UVa10062.java

---
+++ UVa10062.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+
+class main{
+	
+	public static void main(String args[]){
+	
+		Scanner input = new Scanner(System.in);
+		
+		String S = "";
+		int[] count = new int[130];
+		int sum = 0;
+		
+		while(input.hasNext())
+		{
+			S = input.nextLine();
+			for(int i = 0;i < S.length();i++)
+			{
+				sum++;
+				count[S.charAt(i)]++;
+			}
+			
+			for(int k = 1;k<=sum;k++)
+				for(int j = 128;j >= 0;j--)
+				{
+					if(k == count[j])
+						{
+							System.out.println(j + " " + count[j]);
+							count[j] = 0;
+						}
+				}
+			if(input.hasNext())
+				System.out.println();
+		}
+	}
+}
