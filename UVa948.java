73db84a17b564eaa26e8cca7c261891016e9187c
maxi67 <gckc3130@gmail.com>
2016-05-12 18:36:36 +0800

    Create UVa948.java

---
+++ UVa948.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+
+class main{
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		int times = input.nextInt();
+		int A, B,flag = 0;
+		String bin = "";
+		
+		int arr[] = new int[50];
+		arr[0] = 1;
+		arr[1] = 2;
+		for(int i = 2;i < 43; i++)
+			arr[i] = arr[i-1] + arr[i-2];
+
+		while(times-- > 0)
+		{
+			flag = 0;
+			A = input.nextInt();
+			B = A;
+			bin = B + " = ";
+			
+			for(int i = 42;i >= 0;i--)
+			{
+				if(B >= arr[i])
+					{
+						B-=arr[i];
+						bin = bin.concat("1");
+						flag = 1;
+					}
+					
+					else
+					{
+						if(flag == 1)
+							bin = bin.concat("0");
+					}	
+			}
+			bin = bin.concat(" (fib)");
+			System.out.println(bin);
+		}
+	}
+}
