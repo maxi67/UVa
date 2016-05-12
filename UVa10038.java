a55c485ddc5bab694117b9b66985fe543ed7ad7e
maxi67 <gckc3130@gmail.com>
2016-05-12 18:35:56 +0800

    Create UVa10038.java

---
+++ UVa10038.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+
+class main{
+	public static void main(String args[]){
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextInt())
+		{
+			int A = input.nextInt();
+			boolean flag = false;
+			int n[] = new int[100];
+			int m[] = new int[A];
+			
+			for(int j = 1; j < A; j++)
+				m[j] = 1;			
+			
+			for(int i = 0; i < A; i++)
+				n[i] = input.nextInt();
+
+			for(int j = 0, h; j < A - 1; j++)
+			{
+				h = Math.abs(n[j] - n[j+1]);
+				if(h > A-1)
+					continue;		
+				m[h] = 0;
+			}
+			
+			for(int j = 1; j < A; j++)
+				if(m[j] == 1)
+					flag = true;
+			
+			if(!flag)
+				System.out.println("Jolly");
+			else
+				System.out.println("Not jolly");
+		}
+	}
+}
