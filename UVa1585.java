35aa92c8b2cdc3c84faedd83788237bf855847e0
maxi67 <gckc3130@gmail.com>
2016-06-30 00:25:01 +0800

    UVa1585.java
    這題丟瘋狂程設跟UVa online怎樣就是不給過

--- UVa1585.java
+++ UVa1585.java
@@ -4,27 +4,27 @@ class main{
     public static void main(String[] args) {
        
        		Scanner in=new Scanner(System.in);
-           	int a=Integer.valueOf(in.nextLine().charAt(0));
-			for(int q=0;q<a;q++)
-            {
-            	boolean state = true;
-            	int o=0,sum=0;
-            	String S=in.nextLine();
-            	for(int i =0;i<S.length();i++)
+           	int a = Integer.valueOf(in.nextLine().charAt(0));
+		for(int q = 0; q < a; q++)
             	{
-            		if(state == true)
-            			o=0;
-            		if(S.charAt(i)==79) //o
+            		boolean state = true;
+            		int o = 0, sum = 0;
+            		String S = in.nextLine();
+            		for(int i = 0; i < S.length(); i++)
             		{
-            			o++;
-            			sum+=o;
-            			state=false;
+            			if(state == true)
+            				o = 0;
+            			if(S.charAt(i) == 79) //o
+            			{
+            				o++;
+            				sum+=o;
+            				state = false;
+            			}
+            			else //x
+            				state = true;
             		}
-            		else //x
-            			state = true;
+            		System.out.println(sum);
             	}
-            	System.out.println(sum);
-            }
       
-    }
-};
+	 }
+}
