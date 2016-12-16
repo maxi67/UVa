22a8a098e2c5fe51fd1722bf524118fea9648dd7
maxi67 <gckc3130@gmail.com>
2016-12-17 00:51:43 +0800

    Create UVa11332.java
    用String會TL

---
+++ UVa11332.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.util.*;
+
+class Main{
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNext()){
+			long s = input.nextInt();
+			
+			//結束
+			if(s == 0)
+				break;
+			
+			while(s >= 10){
+				long sum = 0;
+				while(s > 0){
+					sum += s%10;
+					s /= 10;
+				}
+				s = sum;
+			}
+			System.out.println(s);
+		}
+	}
+}
