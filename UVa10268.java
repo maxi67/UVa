073f627d95d2cd040c4ded1dc745254e5c2373d4
maxi67 <gckc3130@gmail.com>
2016-12-18 04:04:46 +0800

    Create UVa10268.java

---
+++ UVa10268.java
@@ -0,0 +1,30 @@
+//不知道為什麼UVa一直送RE...
+//CodingFrenzy都可以的
+//算一元多次方程式的導數
+
+import java.lang.*;
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+
+class Main{
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextLine()){
+			
+			int n = Integer.valueOf(input.nextLine());
+			String s = input.nextLine();
+			String[] a = s.split(" ");
+			
+			int deg = a.length - 1;
+			int sum = 0;
+
+			for(int i = deg-1, x = 1, j = 1; j <= deg; i--, j++, x *= n)		
+				sum += x * Integer.valueOf(a[i]) * j;
+			
+			System.out.println(sum);
+		}
+	}
+}
