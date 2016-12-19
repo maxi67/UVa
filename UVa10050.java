0b2cac862747ad741fce73f3292aa6924d44ee3d
maxi67 <gckc3130@gmail.com>
2016-12-19 23:38:56 +0800

    Update UVa10050.java

--- UVa10050.java
+++ UVa10050.java
@@ -6,33 +6,30 @@ class main{
 		Scanner input = new Scanner(System.in);
 		
 		int A = input.nextInt();
-		for(int i = 0;i < A; i++)
+		for(int i = 0; i < A; i++)
 		{
+			int[] state = new int[4000];
 			int count = 0;
 			int B = input.nextInt();
-			int[] state= new int[4000];
 			int C = input.nextInt();
 			
-			for(int j = 0;j < C; j++)
+			for(int j = 0; j < C; j++)
 			{
 				int cost = input.nextInt();
-				for(int k = cost;k <= B;k += cost)
+				for(int k = cost; k <= B; k += cost)
 					state[k] = 1;	
 			}
 			
-			for(int l = 6;l <= B; l += 6)
+			for(int l = 6; l <= B; l += 6)
 			{
-				state[l] = 0;
-				l++;
+				state[l++] = 0;
 				state[l] = 0;	
 			}
 			
-			for(int m = 1;m <= B; m++)
-			{
+			for(int m = 1; m <= B; m++)
 				if(state[m] == 1)
 					count++;
-			}
-
+			
 			System.out.println(count);
 		}
 	}
