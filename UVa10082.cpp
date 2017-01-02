0ccd67438c692ba54fa938d3c2f75e2c9923d9e8
maxi67 <gckc3130@gmail.com>
2017-01-02 23:58:49 +0800

    Update UVa10082.cpp

--- UVa10082.cpp
+++ UVa10082.cpp
@@ -3,21 +3,19 @@
 using namespace std;
 
 int main(){
-	
 	string s;
-	char A[]={"`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"};
+	char A[] = {"`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"};
 
 	while(std::getline(std::cin,s))
 	{
-		for(int i=0;i<s.size();i++)
+		for(int i = 0; i < s.size(); i++)
 		{
-			if(s[i]==' ')
+			if(s[i] == ' ')
 					cout << " ";
-			for(int j=0; A[j] != '\0'; j++)
-				if(s[i]==A[j])
-					cout << A[j-1];
+			for(int j = 0; A[j] != '\0'; j++)
+				if(s[i] == A[j])
+					cout << A[j - 1];
 		}
 		cout << endl;
 	}
-	
 }
