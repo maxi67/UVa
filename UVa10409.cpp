98ce2880ce8bb05d59cbb1dcc8fb0f1a9edd625b
maxi67 <gckc3130@gmail.com>
2017-01-04 16:48:46 +0800

    Update UVa10409.cpp

--- UVa10409.cpp
+++ UVa10409.cpp
@@ -9,30 +9,27 @@ int main(){
 	
 	while(cin >> times, times)
 	{
-		int u=1,d=6,n=2,s=5,w=3,e=4; //init
+		int u = 1 ,d = 6, n = 2, s = 5, w = 3,e = 4; //init
 		while(times--){
 			cin >> a;
 			switch(a[0]){
 			
 				case 'n':
-					temp=u;u=s;s=d;d=n;n=temp; 
+					temp = u; u = s; s = d; d = n; n = temp; 
 					break;
 				case 's': 
-					temp=u;u=n;n=d;d=s;s=temp;
+					temp = u; u = n; n = d; d = s; s = temp;
 					break;
 				case 'e': 
-					temp=u;u=w;w=d;d=e;e=temp;
+					temp = u; u = w; w = d; d = e ;e = temp;
 					break;
 				case 'w': 
-					temp=u;u=e;e=d;d=w;w=temp;
+					temp = u ;u = e; e = d; d = w; w = temp;
 					break;
-				default : break;
-			
+				default : 
+					break;
 			}
 		}
-		
-		cout << u << endl;
-			
+		cout << u << endl;		
 	}
-	
 }
