70af108ea49e5cfafe01ab187126d4e8ee53df96
maxi67 <gckc3130@gmail.com>
2017-03-28 11:55:04 +0800

    Update and rename UVa10929.java to UVa10929_You_can_say_11.java

--- UVa10929_You_can_say_11.java
+++ UVa10929_You_can_say_11.java
@@ -14,10 +14,10 @@ class main{
 			if(S.charAt(0) == 48)
 				break;
 			
-			for(int i = 0;i < S.length();i+=2)
-				sum += (S.charAt(i)-48);
-			for(int j = 1;j < S.length();j+=2)
-				sum -= (S.charAt(j)-48);
+			for(int i = 0; i < S.length(); i+=2)
+				sum += (S.charAt(i) - 48);
+			for(int j = 1; j < S.length(); j+=2)
+				sum -= (S.charAt(j) - 48);
 
 			System.out.print(S);
 			if(sum % 11 == 0)
