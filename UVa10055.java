61d989f927dc300981172e0b96330c4f1dbb4e88
maxi67 <gckc3130@gmail.com>
2017-03-28 10:30:11 +0800

    Update UVa10055.java

--- UVa10055.java
+++ UVa10055.java
@@ -11,6 +11,12 @@ class main{
 			BigInteger A = input.nextBigInteger();
 			BigInteger B = input.nextBigInteger();
 			System.out.println(A.subtract(B).abs());
+			
+			//這題範圍還在long內，也可以這樣寫
+			
+			//long A = input.nextLong();
+			//long B = input.nextLong();
+			//System.out.println(Math.abs(A-B));
 		}
 	}
 }
