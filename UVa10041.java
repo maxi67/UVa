247a834b60ccd0d7c3c5905c4bd9bcd212458919
maxi67 <gckc3130@gmail.com>
2017-12-13 11:50:43 +0800

    Update UVa10041.java

--- UVa10041.java
+++ UVa10041.java
@@ -9,23 +9,18 @@ class main{
 		
 		int a = input.nextInt();
 		int b, sum;
-		while(a-- > 0)
-		{
+		while (a-- > 0){
 			sum = 0;
 			b = input.nextInt();
 			int[] mem = new int[b];
 			int c = b;
-			while(b-- > 0)
-			{
+			while (b-- > 0){
 				mem[b] = input.nextInt();
 			}
 			Arrays.sort(mem);
 			for(int i = 0; i < c; ++i)
-			{
 				sum += Math.abs(mem[c/2] - mem[i]);
-			}
 			System.out.println(sum);
-			
 		}
 	}
 }
