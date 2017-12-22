25e8db69f0e3b2d914d1c04e779c8b9ff07c1201
maxi67 <gckc3130@gmail.com>
2017-12-22 21:50:20 +0800

    Update UVa10071.java

--- UVa10071.java
+++ UVa10071.java
@@ -1,19 +1,12 @@
 import java.util.*;
-import java.lang.*;
 
-class main{
+class Main{
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
-		
-		while(input.hasNext()){
-			
+		while (input.hasNext()){
 			int v = input.nextInt(); //-100 ~ 100
 			int t = input.nextInt(); //   0 ~ 200
-			
-			if(v == 0)
-				System.out.println("0");
-			else
-				System.out.println(2 * t * v);
+			System.out.println(2 * t * v);
 		}
 	}
 }
