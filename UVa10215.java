fbfe21f1fcf59b3b86422d077aa7677b719ca6ac
maxi67 <gckc3130@gmail.com>
2018-03-05 17:41:58 +0800

    Update UVa10215.java

--- UVa10215.java
+++ UVa10215.java
@@ -5,14 +5,11 @@ class Main{
 		Scanner sc = new Scanner(System.in);
 		
 		while (sc.hasNextDouble()){
-			double L = sc.nextDouble();
-			double W = sc.nextDouble();
-			
 			double a = 12;
-			double b = -4 * (L + W);
+			double b = -4*(L + W);
 			double c = L * W;
 			
-			double max = (-b-Math.sqrt(b*b-4*a*c))/(2 * a);
+			double max = (-b - Math.sqrt(b*b - 4*a*c))/(2*a);
 			double min = (W < L ? W : L)/2;
 			System.out.printf("%.3f 0.000 %.3f\n", max, min);
 		}
