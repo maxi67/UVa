64389978292bf9d49cc702e1a419076de25a53f7
maxi67 <gckc3130@gmail.com>
2018-03-05 17:30:42 +0800

    Create 10215.java

---
+++ 10215.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextDouble()){
+			double L = sc.nextDouble();
+			double W = sc.nextDouble();
+			
+			double a = 12;
+			double b = -4 * (L + W);
+			double c = L * W;
+			
+			double max = (-b-Math.sqrt(b*b-4*a*c))/(2 * a);
+			double min = (W < L ? W : L)/2;
+			System.out.printf("%.3f 0.000 %.3f\n", max, min);
+		}
+	}
+}
