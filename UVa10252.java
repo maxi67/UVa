9dbbf54f42ba1179956289e4dcf125fd499def53
maxi67 <gckc3130@gmail.com>
2018-03-09 16:42:52 +0800

    Update UVa10252.java

--- UVa10252.java
+++ UVa10252.java
@@ -1,27 +1,25 @@
-import java.lang.*;
 import java.util.*;
 
-class main{
-	public static void main(String args[])
-	{
-		Scanner input = new Scanner(System.in);
+class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
 		
-		while(input.hasNext()){
-			String s1 = input.nextLine();
-			String s2 = input.nextLine();
+		while (sc.hasNext()){
+			String s1 = sc.nextLine();
+			String s2 = sc.nextLine();
 			int[] c1 = new int[123];
 			int[] c2 = new int[123];
 			
-			//計算個別個數
-			for(int i = 0; i < s1.length(); i++)
+			//統計兩字串各字母出現個數
+			for (int i = 0; i < s1.length(); i++)
 				c1[s1.charAt(i)]++;
-			for(int j = 0; j < s2.length(); j++)
+			for (int j = 0; j < s2.length(); j++)
 				c2[s2.charAt(j)]++;
 				
-			//相同字元時，輸出較少數量那方的個數
-			for(int	k = 97; k < 123; k++)
-					for(int num = 0; num < Math.min(c1[k],c2[k]); num++)
-						System.out.print((char)k);		
+			//數量大於1，輸出較少數量那方的個數
+			for (int k = 97; k <= 122; k++)
+				for(int num = 0; num < Math.min(c1[k], c2[k]); num++)
+					System.out.print((char)k);		
 				
 			System.out.println();
 		}
