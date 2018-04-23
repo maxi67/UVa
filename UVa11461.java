6a10af66853ef742481edddb4c166bea75d4b958
maxi67 <gckc3130@gmail.com>
2018-04-23 23:20:59 +0800

    Update UVa11461.java

--- UVa11461.java
+++ UVa11461.java
@@ -1,44 +1,42 @@
-import java.lang.*;
 import java.util.*;
 
-class main{
-	public static void main(String args[])
-	{
+public class Main {
+	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
 		long square[] = new long[1001];
-		for(int i = 1; i <= 1000; i++)
+		for (int i = 1; i <= 1000; i++)
 			square[i] = i * i;
 		
-		while(input.hasNextLine()){	
+		while (input.hasNextLine()){	
 			int a = input.nextInt();
 			int b = input.nextInt();
 			int count = 0;
 			int in = 1;
 			int in2 = 1000;
 				
-			if(a == 0 & b == 0)
+			if (a == 0 & b == 0)
 				break;
 				
-			for(; in < 1001; in++)
-				if(a <= square[in]){
+			for (; in < 1001; in++)
+				if (a <= square[in]) {
 					in--;
 					break;
 				}
 						
-			for(; in2 > 0; in2--)
-				if(b >= square[in2])
+			for (; in2 > 0; in2--)
+				if (b >= square[in2])
 					break;	
 			count = in2 - in;
 					
-			/*	本來的寫法 -> TL
-				for(int i = a; i <= b; i++)
-					for(int index = 1; index < 1001; index++){
-						if(i == square[index]){
+			/*	本來的寫法 -> TLE
+				for (int i = a; i <= b; i++)
+					for (int index = 1; index < 1001; index++){
+						if (i == square[index]) {
 							count++;
 							index = 1001;
 						}
 					}
-					*/
+			*/
 			System.out.println(count);
 		}
 	}
