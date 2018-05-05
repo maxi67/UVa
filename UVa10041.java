e02a8d9990e25ac6d0eadcc895baca2c93b283af
maxi67 <gckc3130@gmail.com>
2018-05-05 12:46:20 +0800

    Update UVa10041.java

--- UVa10041.java
+++ UVa10041.java
@@ -1,25 +1,20 @@
-//找中位數，算距離和
 import java.util.*;
-import java.lang.*;
 
-class Main{
-	public static void main(String args[]){
-	
-		Scanner input = new Scanner(System.in);
-		
-		int a = input.nextInt();
-		int b, sum;
-		while (a-- > 0){
-			sum = 0;
-			b = input.nextInt();
-			int[] mem = new int[b];
-			int c = b;
-			while (b-- > 0){
-				mem[b] = input.nextInt();
-			}
-			Arrays.sort(mem);
-			for(int i = 0; i < c; ++i)
-				sum += Math.abs(mem[c/2] - mem[i]);
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int r = sc.nextInt();
+			int arr[] = new int[r];
+			for (int i = 0; i < r; i++) 
+				arr[i] = sc.nextInt();
+			
+			Arrays.sort(arr);
+			int sum = 0;
+			for (int i: arr)
+				sum += Math.abs(arr[r/2] - i);
+			
 			System.out.println(sum);
 		}
 	}
