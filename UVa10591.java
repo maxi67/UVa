37f6eddcd55ce2cbe7a0228008bedf3e417163df
maxi67 <gckc3130@gmail.com>
2018-05-10 12:56:31 +0800

    Create UVa10591.java

---
+++ UVa10591.java
@@ -0,0 +1,41 @@
+package uva10000_10999;
+
+import java.util.*;
+
+public class UVa10591 {
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		int t = Integer.parseInt(sc.nextLine());
+		
+		for (int i = 1; i <= t; i++) {	
+			boolean happy = true;
+			Set<Integer> set = new HashSet<Integer>();
+			
+			int N = Integer.parseInt(sc.nextLine());
+			set.add(N);
+			int sum = N;
+			
+			while (sum != 1) {		
+				int N1 = sum;
+				sum = 0;
+				
+				while ( N1 > 0 ){
+					sum += (N1 % 10) * (N1 % 10);
+					N1 /= 10;
+				}
+
+				if (set.contains(sum)){
+			          happy = false;
+			          break;
+			    }		
+				set.add(sum);
+			}
+			
+			if (happy)
+				System.out.println("Case #" + i + ": " + N + " is a Happy number.");
+			else
+				System.out.println("Case #" + i + ": " + N + " is an Unhappy number.");
+		}
+	}
+}
