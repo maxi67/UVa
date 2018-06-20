77545ffbe8d430eeefd9d33415ca0566d38bf001
maxi67 <gckc3130@gmail.com>
2018-06-20 16:55:20 +0800

    Create UVa1730.java

---
+++ UVa1730.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			long N = sc.nextLong();
+			if (N == 0)
+				break;
+			
+			long sum = 0;
+			long ans = 0, times, R, L = 1;
+			long count = 0;
+			
+			while (L <= N) {
+	            times = N / L;
+	            R = N / times;
+	            ans += times * ((L + R) * (R - L + 1) / 2);
+	            L = R + 1;
+	            count++;
+	        }
+			System.out.println(ans - 1);
+		}
+	}
+}
