800886451addba0d55b0914db14e15150dc303a1
maxi67 <gckc3130@gmail.com>
2018-06-27 21:38:06 +0800

    Update UVa1585.java

--- UVa1585.java
+++ UVa1585.java
@@ -1,21 +1,18 @@
 import java.util.*;
-import java.lang.*;
-class main{
+
+class Main{
     public static void main(String[] args) {
        
        		Scanner in=new Scanner(System.in);
            	int a = Integer.valueOf(in.nextLine().charAt(0));
-		for(int q = 0; q < a; q++)
-            	{
+		for (int q = 0; q < a; q++) {
             		boolean state = true;
             		int o = 0, sum = 0;
             		String S = in.nextLine();
-            		for(int i = 0; i < S.length(); i++)
-            		{
-            			if(state == true)
+            		for (int i = 0; i < S.length(); i++) {
+            			if (state == true)
             				o = 0;
-            			if(S.charAt(i) == 79) //o
-            			{
+            			if (S.charAt(i) == 79) {//o 
             				o++;
             				sum+=o;
             				state = false;
