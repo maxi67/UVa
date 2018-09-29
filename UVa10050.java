4aecdf27dfdf1d5625804e92f2ecfdf10bb44302
maxi67 <gckc3130@gmail.com>
2018-09-29 17:21:01 +0800

    Update UVa10050.java

--- UVa10050.java
+++ UVa10050.java
@@ -1,30 +1,30 @@
 import java.util.*;
-import java.lang.*;
 
-class Main{
+public class Main {
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
 		
-		int A = input.nextInt();
-		for(int i = 0; i < A; i++){
+		int T = input.nextInt();
+		for (int i = 0; i < T; i++){
 			int[] state = new int[4000];
 			int count = 0;
-			int B = input.nextInt();
-			int C = input.nextInt();
+			int N = input.nextInt();
+			int P = input.nextInt();
 			
-			for(int j = 0; j < C; j++){
+			for (int j = 0; j < P; j++){
 				int cost = input.nextInt();
-				for(int k = cost; k <= B; k += cost)
+				for (int k = cost; k <= N; k += cost)
 					state[k] = 1;	
 			}
 			
-			for(int l = 6; l <= B; l += 6){
-				state[l++] = 0;
-				state[l] = 0;	
+			//Friday & Saturday
+			for (int j = 6; j <= N; j += 6){
+				state[j++] = 0;
+				state[j] = 0;	
 			}
 			
-			for(int m = 1; m <= B; m++)
-				if(state[m] == 1)
+			for (int m = 1; m <= N; m++)
+				if (state[m] == 1)
 					count++;
 			
 			System.out.println(count);
