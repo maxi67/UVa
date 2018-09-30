8788309575d0f5dcfa23445cb812f5dcb36ea2a5
maxi67 <gckc3130@gmail.com>
2018-09-30 13:14:14 +0800

    Update UVa10489.java

--- UVa10489.java
+++ UVa10489.java
@@ -22,6 +22,8 @@ public class Main {
 		}
 	}
 }
+//============ Method 2 ============
+//import java.util.*;
 //public class Main {
 //	public static void main(String args[]) {
 //		Scanner sc = new Scanner(System.in);
