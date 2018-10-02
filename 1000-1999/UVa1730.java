6900b14f3f3deddecf913dcbd62e6a8aaaada04c
maxi67 <gckc3130@gmail.com>
2018-10-02 16:46:55 +0800

    Update UVa1730.java

--- 1000-1999/UVa1730.java
+++ 1000-1999/UVa1730.java
@@ -7,17 +7,14 @@ public class Main {
 			long N = sc.nextLong();
 			if (N == 0)
 				break;
-			
-			long sum = 0;
+
 			long ans = 0, times, R, L = 1;
-			long count = 0;
-			
+
 			while (L <= N) {
 				times = N / L;
 				R = N / times;
 				ans += times * ((L + R) * (R - L + 1) / 2);
 				L = R + 1;
-				count++;
 			}
 			System.out.println(ans - 1);
 		}
